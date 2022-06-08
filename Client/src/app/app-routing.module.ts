5b9cae817bac390100d9a053cc2b221b311f6ff5
Sergey Boykov <boykov.sergei@gmail.com>
2022-06-08 14:17:01 +0500

    Client - Pazzazz (improvements)

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -4,17 +4,16 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
+  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
+  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
+  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
   // Shop module is only going to be activated and loaded when we access the shop path.
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), 
+    data: {breadcrumb: 'Shop'}},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
---
+++ Client/src/app/core/services/busy.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequestCount = 0;
+
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+  busy() {
+    this.busyRequestCount++;
+    this.spinnerService.show(undefined, {
+      type: 'timer',
+      size: 'medium',
+      bdColor: 'rgba(255, 255, 225, 0.7)',
+      color: '#000000'
+    });
+  }
+
+  idle() {
+    this.busyRequestCount--;
+    if (this.busyRequestCount <= 0) {
+      this.busyRequestCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+}
